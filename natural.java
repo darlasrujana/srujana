84813a414942e24e42076c0a713d8c574734f109
darlasrujana <41771819+darlasrujana@users.noreply.github.com>
2018-08-02 02:22:54 -0700

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class natural
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num = 2, sum = 0;
+
+		for(int i = 1; i <= num; ++i)
+		{
+            		sum += i;
+		}
+
+		 System.out.println("Sum = " + sum);
+	 }
+}
