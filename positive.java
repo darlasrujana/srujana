0dffc9f071e26ee851e1bf434493d846e2e10abe
darlasrujana <41771819+darlasrujana@users.noreply.github.com>
2018-08-13 09:34:35 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -3,24 +3,23 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class positive
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		 int number=109;
-        if(number > 0)
+		Scanner sc=new Scanner(System.in);
+		int number=sc.nextInt();
+	        if(number > 0)
         {
-            System.out.println(number+" is a positive number");
+            System.out.println("positive");
         }
         else if(number < 0)
         {
-            System.out.println(number+" is a negative number");
+            System.out.println("negative");
         }
         else
         {
-            System.out.println(number+" is neither positive nor negative");
+            System.out.println("zero");
         }
 	}
 }
