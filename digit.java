53cc7681ffa34cebb0e678d469f33645e31753cf
darlasrujana <41771819+darlasrujana@users.noreply.github.com>
2018-08-14 12:50:37 +0530

    Update digit.java

--- digit.java
+++ digit.java
@@ -5,18 +5,18 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class digit
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int count = 0, num = 548;
-
-        while(num != 0)
-        {
-            num /= 10;
-            ++count;
-        }
-
-        System.out.println("Number of digits: " + count);
+	            Scanner sc=new Scanner(System.in);
+            	int n=sc.nextInt();
+		int  count = 0;
+		while(n>0)
+		{
+		   n=n/10;
+	           	   count++;
+		}
+		System.out.println(count);	
 	}
 }
