58a9266ea8348e2f0babc7edc0b0fc0e0323f33f
darlasrujana <41771819+darlasrujana@users.noreply.github.com>
2018-08-14 11:10:03 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class prime
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -26,6 +26,6 @@ class prime
 		else
 		{
 			System.out.println("no");
-		}
+	    }
 	}
 }
