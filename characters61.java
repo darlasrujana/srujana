c799ed4c08ebf4844f99c9aeda958f140be2cdd9
darlasrujana <41771819+darlasrujana@users.noreply.github.com>
2018-08-28 11:20:33 +0530

    Create characters61.java

---
+++ characters61.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class characters61
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			 int n;
+	     Scanner sc=new Scanner(System.in);
+	     String s=sc.next();
+	     n=sc.nextInt();
+	     char a[]=s.toCharArray();
+	     for(int i=0;i<n;i++)
+	     {
+	 	      System.out.print(a[i]);
+	     }
+	 }
+}
