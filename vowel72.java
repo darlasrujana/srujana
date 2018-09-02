a149fdf4dedc952de2b694ccb89ce4f93bee0859
darlasrujana <41771819+darlasrujana@users.noreply.github.com>
2018-09-02 12:26:52 +0530

    Create vowel72.java

---
+++ vowel72.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class vowel72
+{
+	   public static void main (String[] args) throws java.lang.Exception
+	   {
+		     Scanner sc=new Scanner(System.in);
+     	   char c=sc.next().charAt(0);
+	       if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+		     {
+		         System.out.println("yes");
+		     }
+ 		     else
+		     {
+		         System.out.println("no");
+	       }
+      }
+}
